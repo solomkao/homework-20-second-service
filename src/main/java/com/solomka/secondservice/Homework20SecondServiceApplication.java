47e555bfa3a43b01a051a0280f49176a8860e480
Oksana <solomkao@gmail.com>
2021-02-18 22:51:38 +0200

    Initial commit

---
+++ src/main/java/com/solomka/secondservice/Homework20SecondServiceApplication.java
@@ -0,0 +1,13 @@
+package com.solomka.secondservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework20SecondServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework20SecondServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solomka/secondservice/Homework20SecondServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomka.secondservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework20SecondServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
